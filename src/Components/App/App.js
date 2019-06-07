668353739d23e480e7021cdf2201c62bde7a7ff4
Chris <cjmarsh725@gmail.com>
2019-06-07 16:04:09 -0700

    improved weather card styling

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -18,6 +18,7 @@ class App extends Component {
     return (
       <div className="App">
         {this.state.weatherData.map((day, i) => {
+          if (!day.isDaytime) return null;
           return (
             <Card {...day} key={i}/>
           );
--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -4,10 +4,14 @@ import './Card.css';
 const Card = props => {
   return (
     <div className="card-container">
-      <div className="card-name">{props.name}</div>
-      <div>{props.temperature}&deg;</div>
-      <div><img src={props.icon} alt={props.shortForecast}/></div>
-      <div>{props.shortForecast}</div>
+      <div className="img-container">
+        <img src={props.icon} alt={props.shortForecast}/>
+      </div>
+      <div className="description-container">
+        <div className="description-name">{props.name}</div>
+        <div className="description-temp">{props.temperature}&deg;</div>
+        <div className="description-forecast">{props.shortForecast}</div>
+      </div>
     </div>
   );
 };
